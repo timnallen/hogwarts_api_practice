50720f48b171007fe84e474f587f24c86cd708b7
Tim Allen <tnelsallen3@gmail.com>
2019-04-03 13:17:22 -0600

    Adds feature test for page, search controller, view for search results

---
+++ spec/features/user_sees_their_house_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+feature 'as a User' do
+    it 'shows me my house members' do
+    visit '/'
+
+    select 'Slytherin', from: 'house'
+
+    click_on 'Get Students'
+
+    expect(current_path).to eq('/search')
+    expect(current_url).to include('house=Slytherin')
+
+    expect(page).to have_content('22 Students')
+
+    expect(page).to have_css('.member', count: 22)
+
+    within(first('.member')) do
+      expect(page).to have_css('.name')
+      expect(page).to have_css('.id')
+    end
+  end
+end
