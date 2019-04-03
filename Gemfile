1dc4bbf10a3ea2a4fe61bcd9f60019ee0d2991a2
Tim Allen <tnelsallen3@gmail.com>
2019-04-03 13:48:06 -0600

    Adds faraday steps to pull students info out of hogwarts api

--- Gemfile
+++ Gemfile
@@ -8,6 +8,8 @@ gem 'pg', '>= 0.18', '< 2.0'
 gem 'puma', '~> 3.11'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
+gem 'faraday'
+gem 'figaro'
 
 gem 'jbuilder', '~> 2.5'
 gem 'bootsnap', '>= 1.1.0', require: false
--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -8,6 +8,30 @@ class SearchFacade
   end
 
   def students
-    []
+    service.student_info
+    student_info = get_json("/api/v1/house/#{@house}#{auth}")[:data][0][:attributes][:students]
+  end
+
+  private
+
+  def service
+  end
+
+  def auth
+    "?api_key=#{ENV['HOGWARTS_API_KEY']}"
+  end
+
+  def get_json(url)
+    JSON.parse(response(url).body, symbolize_names: true)
+  end
+
+  def response(url)
+    conn.get(url)
+  end
+
+  def conn
+    Faraday.new(url: 'http://hogwarts-it.herokuapp.com') do |faraday|
+      faraday.adapter  Faraday.default_adapter
+    end
   end
 end
